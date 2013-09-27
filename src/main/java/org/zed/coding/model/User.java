7ba3d20235eda76df8d3605fb70faeb14024ff29
Arnaud Zdziobeck <arnaud.zdziobeck@atos.net>
2013-09-27 10:34:27 +0200

    First commit

---
+++ src/main/java/org/zed/coding/model/User.java
@@ -0,0 +1,25 @@
+package org.zed.coding.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    private int id;
+
+    public User() {
+        super();
+    }
+
+    @Id
+    @GeneratedValue
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
